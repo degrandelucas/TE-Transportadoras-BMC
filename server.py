67ab190873ccc75e4ca3f984a7b7190016ede2ce
Lucas Degrande <lucasdegrande15@gmail.com>
2024-04-22 20:49:05 -0300

    subindo chave key, porem com repositorio privado

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from sheets_api import get_data_from_sheet
 app = Flask(__name__)
 
 # Substitua 'your_api_key_here' pela sua chave de API real da OpenAI.
-openai_api_key = os.getenv("OPENAI_API_KEY", "your_api_key_here")
+openai_api_key = os.getenv("OPENAI_API_KEY", "sk-proj-QcKDZF9MdpycqMHgLKCTT3BlbkFJLbOTG2B51HgrEq8jFi1D")
 
 @app.route('/consultar-transportadora', methods=['POST', 'OPTIONS'])
 def consultar_transportadora():
